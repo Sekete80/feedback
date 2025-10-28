efc98a4526e2b9f7e085fd03928dfcdd80613044
nthethe sekete <nthethesekete280@gmail>
2025-10-28 20:01:47 +0200

    2.1

--- backend/db.js
+++ backend/db.js
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // important for Render
+      },
+    },
   }
 );
 
